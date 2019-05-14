bb5e4b35defa3faea7372bdbc33c647483d7d4bf
Jeanpa03 <50465690+Jeanpa03@users.noreply.github.com>
2019-05-14 17:42:40 -0400

    cambios
    phyton aprendiendo

--- MostrarTablaMultiplicacion.py
+++ MostrarTablaMultiplicacion.py
@@ -1,12 +1,21 @@
 def MostrarTabla(tablaMultiplicar):
     multiplicando = 1
+    suma = 0
 
     while multiplicando <=12:
         nuevoValor = tablaMultiplicar * multiplicando
         print("{0}*{1}={2}".format(tablaMultiplicar, multiplicando, nuevoValor))
         multiplicando = multiplicando + 1
+        suma= suma + nuevoValor
+        
+    return suma  
+
+    
         
 
 
 tabla = int(input("digite la tabla que desea: "))
-MostrarTabla(tabla)
+
+Resultado = MostrarTabla(tabla)
+
+print (Resultado)
